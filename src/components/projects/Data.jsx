8d78bbdef69fcc17b738a161f40282d3bc1d7ac8
GaryVH <gary.vanhecke@gmail.com>
2023-10-31 13:44:03 +0100

    changes started

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -2,39 +2,62 @@ import Project1 from "../../assets/project1.webp";
 import Project2 from "../../assets/project2.webp";
 import Project3 from "../../assets/project3.webp";
 
-
 export const projectsData = [
-    {
-        id: 1,
-        image: Project1,
-        title: "Project 1",
-        category: "A",
-    },
-    {
-        id: 2,
-        image: Project2,
-        title: "Project 2",
-        category: "B",
-    },
-    {
-        id: 3,
-        image: Project3,
-        title: "Project 3",
-        category: "C",
-    },
+  {
+    id: 1,
+    image: Project1,
+    title: "Projet 1 : Booki",
+    category: "A",
+  },
+  {
+    id: 2,
+    image: Project1,
+    title: "Projet 2 : Ohmyfood",
+    category: "A",
+  },
+  {
+    id: 3,
+    image: Project3,
+    title: "Projet 3 : La panthère",
+    category: "A",
+  },
+  {
+    id: 4,
+    image: Project3,
+    title: "Projet 4 : Kanap",
+    category: "A",
+  },
+  {
+    id: 5,
+    image: Project3,
+    title: "Projet 5 : Piiquante",
+    category: "B",
+  },
+  {
+    id: 6,
+    image: Project3,
+    title: "Projet 6 : Kasa",
+    category: "C",
+  },
+  {
+    id: 7,
+    image: Project3,
+    title: "Projet 7 : Ancistrus",
+    category: "B",
+  },
 ];
 
 export const projectsNav = [
-    {
-        name: "All",
-    },
-    {
-        name: "A",
-    },
-    {
-        name: "B",
-    },
-    {
-        name: "C",
-    },
+  {
+    name: "All",
+  },
+  {
+    name: "A",
+  },
+  {
+    name: "B",
+  },
+  {
+    name: "C",
+  },
 ];
